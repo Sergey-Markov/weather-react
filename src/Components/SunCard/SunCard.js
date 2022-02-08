3694b1bce0907079e15e7c690ba9f80b5ba34680
Sergey-Markov <1nativefl@gmail.com>
2022-02-08 12:22:46 +0200

    add func dateBuilder to utils and Card

--- src/Components/SunCard/SunCard.js
+++ src/Components/SunCard/SunCard.js
@@ -1,8 +1,35 @@
 import s from "./SunCard.module.css";
+import { dateBuilder } from "../../utils/dateBuilder";
+
 export default function SunCard() {
   return (
     <div className={`${s.field} + ${s}`}>
-      <p>My Card</p>
+      <div className={s.card_head}>
+        <div>
+          <p className={s.city}>Cherkasy, UA</p>
+          <p className={s.date}>{dateBuilder(new Date())}</p>
+        </div>
+        <div>
+          <span>IMGSunny</span>
+          <span>Sunny</span>
+        </div>
+      </div>
+
+      <div className={s.card_chart}>My charts</div>
+
+      <div className={s.card_footer}>
+        <div>
+          <p>
+            <span className={s.temperature}>16</span> °С | °F
+          </p>
+          <p className={s.temperature_feels}>Feels like: +22 °С </p>
+        </div>
+        <div className={s.weather_other}>
+          <p className={s.weather_other_text}>Wind: 16 m/s</p>
+          <p className={s.weather_other_text}>Humidity: 13%</p>
+          <p className={s.weather_other_text}>Pressure: 600Pa</p>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/utils/dateBuilder.js
@@ -0,0 +1,26 @@
+const dateBuilder = (d) => {
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let time = d.toLocaleTimeString().slice(0, -3);
+
+  return `${day}, ${date} ${month}, ${time}`;
+};
+
+export { dateBuilder };
