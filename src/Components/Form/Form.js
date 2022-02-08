2874ded2e6c4e25413e843b7abf841b180caf9e0
Sergey-Markov <1nativefl@gmail.com>
2022-02-08 12:23:28 +0200

    add button and some css

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -2,8 +2,17 @@ import s from "./Form.module.css";
 
 export default function Form() {
   return (
-    <div>
-      <input className={s.input} type="text" placeholder="Search..." />
+    <div className={s.field}>
+      <input
+        name="city"
+        className={s.input}
+        type="text"
+        placeholder="Search..."
+        autoComplete="on"
+      />
+      <button className={s.button} type="submit">
+        Add
+      </button>
     </div>
   );
 }
